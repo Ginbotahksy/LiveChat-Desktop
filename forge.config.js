9dd7f2d94ba34fea8cee94fe0fe5077f78de1f90
Ginbo <darckwil15@gmail.com>
2026-01-02 00:42:35 +0100

    dans le bon

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,54 @@
 const webpack = require('webpack');
 
 module.exports = {
-  // ... packagerConfig
+  packagerConfig: {
+    extendInfo: {
+      LSUIElement: "1", // Mode agent pour macOS
+    },
+  },
+  rebuildConfig: {},
+  makers: [
+    { name: '@electron-forge/maker-squirrel' },
+    { name: '@electron-forge/maker-zip', platforms: ['darwin'] },
+    { name: '@electron-forge/maker-deb', config: {} },
+    { name: '@electron-forge/maker-rpm', config: {} },
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-webpack',
       config: {
         mainConfig: {
           entry: './src/main.js',
+          module: {
+            rules: require('./webpack.rules.js'), // Assure-toi d'avoir ce fichier ou vide []
+          },
           plugins: [
             new webpack.DefinePlugin({
-              'process.env.ADDRESS': JSON.stringify(process.env.ADDRESS)
-            })
+              'process.env.ADDRESS': JSON.stringify(process.env.ADDRESS),
+            }),
           ],
         },
         renderer: {
           config: {
             entry: './src/renderer.js',
+            module: {
+              rules: require('./webpack.rules.js'),
+            },
             plugins: [
               new webpack.DefinePlugin({
-                'process.env.ADDRESS': JSON.stringify(process.env.ADDRESS)
-              })
-            ]
+                'process.env.ADDRESS': JSON.stringify(process.env.ADDRESS),
+              }),
+            ],
           },
-        }
-      }
-    }
-  ]
+          entryPoints: [
+            {
+              html: './src/index.html',
+              js: './src/renderer.js',
+              name: 'main_window',
+            },
+          ],
+        },
+      },
+    },
+  ],
 };
